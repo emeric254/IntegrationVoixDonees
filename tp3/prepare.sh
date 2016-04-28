98e929e247d8602ca646294ba374236746241f73
toast254 <toast254@users.noreply.github.com>
2016-04-28 16:43:28 +0200

    Create prepare.sh

---
+++ tp3/prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install asterisk
+apt-get install asterisk-prompt-fr-proformatique
+
+# sip phone(s)
+#apt-get install twinkle
+#apt-get install ekiga
+#apt-get install linphone
