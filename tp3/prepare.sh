628b113d7dc2c9026dd1d47b9e00cb40fcf50aec
Tosi Émeric <emeric254@users.noreply.github.com>
2016-05-11 16:30:33 +0200

    Update prepare.sh

--- tp3/prepare.sh
+++ tp3/prepare.sh
@@ -9,3 +9,11 @@ apt-get install asterisk-prompt-fr-proformatique
 #apt-get install twinkle
 #apt-get install ekiga
 #apt-get install linphone
+
+
+killall dhclient
+
+ip a f dev eth0
+ip r f dev eth0
+
+ip a a 1.1.0.1/16 dev eth0
